f7e44afae4e75ab0acf3199c9f1b9bbb5c89a289
98aj <103376356+98aj@users.noreply.github.com>
2023-03-17 21:36:06 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,15 @@ const App = () => {
   return (
     <div id="main">
                {/* Do not remove the main div */}
+
+      <ol key='relativeList'>
+        <li key='relativeListItem1'>Anna</li>
+<li key='relativeListItem2'>Jhon</li>
+<li key='relativeListItem3'>Don</li>
+<li key='relativeListItem4'>Boom</li>
+<li key='relativeListItem5'>Tom</li>
+<li key='relativeListItem6'>Jerry</li>
+      </ol>
     </div>
   )
 }
